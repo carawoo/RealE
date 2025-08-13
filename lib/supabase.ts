a126e9d6edcba5eee5e5475fe5257af39da42c83
carawoo <carawoo96@gmail.com>
2025-08-13 21:33:16 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,14 @@
-// /lib/supabase.ts
+// lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE!, // 서버 전용 키
+  process.env.SUPABASE_SERVICE_ROLE!,
   { auth: { persistSession: false } }
 );
 
 export const supabasePublic = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // 읽기용
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   { auth: { persistSession: false } }
 );
